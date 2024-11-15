f0a774ec11f44dba49a340f1b4e02d675eeae271
Igor Nemykin <igor.nemykin@dhl.com>
2024-11-15 15:52:09 +0100

    update raceCondition/raceCondition.go

--- raceCondition/raceCondition.go
+++ raceCondition/raceCondition.go
@@ -6,17 +6,7 @@ import (
 )
 
 func main() {
-	var counter int
-
-	for i := 0; i < 10; i++ {
-		go func() {
-			for j := 0; j < 1000; j++ {
-				counter++
-			}
-		}()
-	}
-
+	go fmt.Println("I'm the First!")
+	go fmt.Println("I'm the Second!")
 	time.Sleep(1 * time.Second)
-
-	fmt.Println("Final counter value:", counter)
 }
